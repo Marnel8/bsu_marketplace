1637b9b2a9a49c59b6721080ba96a6377d1b8fc4
Marnel <mharnhelvalentin@gmail.com>
2024-12-04 09:09:07 +0800

    Update api.ts

--- utils/api.ts
+++ utils/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 const instance = axios.create({
   // baseURL: "http://localhost:5000/api/v1",
   baseURL: "https://batstateu-marketplace.parallaxed.ph/api/v1",
+  // withCredentials: true
 });
 
 instance.interceptors.response.use(
